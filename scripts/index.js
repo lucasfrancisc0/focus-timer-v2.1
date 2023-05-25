03c8391af3dba76aa08726f2ad621c44c26f9447
lucas <lucasarnoldd@gmail.com>
2023-05-25 15:12:21 -0300

    update function pause

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const minutesDisplay = document.querySelector('#minutes')
 const secondsDisplay = document.querySelector('#seconds')
 const buttonPlayTimer = document.querySelector('#buttonPlay')
+const buttonPauseTimer = document.querySelector('#buttonPause')
 const buttonStopTimer = document.querySelector('#buttonStop')
 const buttonAddTime = document.querySelector('#buttonAddTime')
 const buttonReduceTime = document.querySelector('#buttonReduceTime')
@@ -9,11 +10,26 @@ let timerTimeOut;
 
 function timerControl () {
 
+  function pause () {
+    buttonPauseTimer.style.display = "none"
+    buttonPlayTimer.style.display = "block"
+  }
+
+  function play () {
+    buttonPlayTimer.style.display = "none"
+    buttonPauseTimer.style.display = "block"
+  }
+
   function updateDisplay (minutes, seconds) {
     minutesDisplay.textContent = String(minutes).padStart(2, "0")
     secondsDisplay.textContent = String(seconds).padStart(2, "0")
   }
 
+  function resetDisplay () {
+    let minutes = 0
+    let seconds = 0
+    updateDisplay(minutes, seconds)
+  }
 
 
   function countdown (minutesDisplay, secondsDisplay) {
@@ -63,7 +79,10 @@ function timerControl () {
     addTime,
     reduceTime,
     countdown,
-    timerTimeOut
+    timerTimeOut,
+    play,
+    pause,
+    resetDisplay
   }
 
 
@@ -72,11 +91,19 @@ function timerControl () {
 const timer = timerControl()
 
 buttonPlayTimer.addEventListener('click', () => {
+  timer.play()
   timer.countdown(minutesDisplay, secondsDisplay)
+})
+
+buttonPauseTimer.addEventListener('click', () =>{
+  timer.pause()
+  clearTimeout(timerTimeOut)
 })
 
 buttonStopTimer.addEventListener('click', () =>{
   clearTimeout(timerTimeOut)
+  timer.resetDisplay()
+  timer.pause()
 })
 
 buttonAddTime.addEventListener('click', () => {
